0fe53d7ac95f568bec62809f63e20b48b240dbf7
Moh Alief Rizky Ramadhan <alief.ramadhan@pasarpolis.com>
2025-04-29 22:27:15 +0700

    fix: change text color

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ function App() {
     <div data-theme='valentine' className='w-screen h-screen flex justify-center items-center bg-pink-900'>
       {!started ? (
         <div className="text-center space-y-6">
-          <h1 className="text-4xl font-bold text-pink-700">💖 Teka Teki Wota 💖</h1>
+          <h1 className="text-4xl font-bold text-[#fff089]">💖 Teka Teki Wota 💖</h1>
           {/* <h1 className="text-4xl font-bold text-pink-700">🎀 Teka Teki Wota 🎀</h1> */}
-          <p className="text-lg text-pink-600">Mari kita lihat seberapa tahukah kalian tentang JKT48</p>
+          <p className="text-lg text-[#fff089]">Mari kita lihat seberapa tahukah kalian tentang JKT48</p>
           <button 
             onClick={() => setStarted(true)}
             className="btn btn-primary btn-lg"
